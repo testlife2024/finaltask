05e4e23e771e31ebe9b52bd6ebf968cf16471709
itisa228 <pulnovaalisa@gmail.com>
2025-07-31 11:23:33 +0700

    Update test-ui.py

--- test-ui.py
+++ test-ui.py
@@ -33,8 +33,8 @@ def test_search_product_neg():
     result_txt = f'Ничего не нашлось по запросу «{requests}»'
     driver.find_element(By.CSS_SELECTOR, '#searchInput').send_keys(requests)
     driver.find_element(By.CSS_SELECTOR, '#applySearchBtn').click()
-    WebDriverWait(driver,5).until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#app > div:nth-child(5) > div.catalog-page.catalog-page--non-search > div > div.catalog-page__not-found.not-found-search > div > h1'), result_txt))
-    assert driver.find_element(By.CSS_SELECTOR,'#app > div:nth-child(5) > div.catalog-page.catalog-page--non-search > div > div.catalog-page__not-found.not-found-search > div > h1').text == result_txt
+    WebDriverWait(driver,5).until(EC.text_to_be_present_in_element((By.XPATH, '//*[@id="appReactRoot"]/div/div[1]/div[1]/div/h1'), result_txt))
+    assert driver.find_element(By.XPATH,'//*[@id="appReactRoot"]/div/div[1]/div[1]/div/h1').text == result_txt
     driver.quit()
 
 
@@ -43,10 +43,9 @@ def test_directions():
                       Страница с результатами по запрашиваемому направлению отображается
     '''
     WB.WB(driver).wb_avia()
-    driver.find_element(By.TAG_NAME, 'body')
     driver.find_element(By.CSS_SELECTOR,'#__next > main > div:nth-child(1) > div > div:nth-child(3) > div > div:nth-child(1) > div').click()
-    WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.CSS_SELECTOR,'#__next > main > div:nth-child(1) > div > div > div > div.LjquDB-1751270365211 > div.t_w8QQ-1751270365211 > div.shared__tag-button-module__tagButtonGroup--9aBd8_1748530590763 > div.shared__tag-button-module__button--Qv07L_1748530590763.shared__tag-button-module__button__tab--rVLma_1748530590763.shared__tag-button-module__selected--hP5R8_1748530590763.undefined > span')))
-    element = driver.find_element(By.CSS_SELECTOR,'#__next > main > div:nth-child(1) > div > div > div > div.LjquDB-1751270365211 > div.t_w8QQ-1751270365211 > div.shared__tag-button-module__tagButtonGroup--9aBd8_1748530590763 > div.shared__tag-button-module__button--Qv07L_1748530590763.shared__tag-button-module__button__tab--rVLma_1748530590763.shared__tag-button-module__selected--hP5R8_1748530590763.undefined > span')
+    WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.XPATH,'//*[@id="__next"]/main/div[1]/div/div/div/div[3]/div[2]/div[1]/div[1]/span')))
+    element = driver.find_element(By.XPATH,'//*[@id="__next"]/main/div[1]/div/div/div/div[3]/div[2]/div[1]/div[1]/span')
     assert element.is_displayed() == True
     driver.quit()
 
@@ -56,9 +55,10 @@ def test_content_page():
                       Страница с результатами по запрашиваемой теме контента отображается
     '''
     WB.WB(driver).wibes()
+    WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.XPATH,'/html/body/div[2]/div[2]/aside/section/nav/ul/li[2]/button')))
     requests = 'еда'
-    driver.find_element(By.CSS_SELECTOR,'body > div.layout > aside > section > nav > ul > li:nth-child(2) > button').click()
-    driver.find_element(By.XPATH,'//*[@id="radix-:Rifekva:"]/div/div[1]/div[2]/div/input').send_keys(requests)
+    driver.find_element(By.CSS_SELECTOR,'body > div.layout_layout__bQuUa > div.layout_layoutContent__aBhHY > aside > section > nav > ul > li:nth-child(2) > button').click()
+    driver.find_element(By.XPATH,'//*[@id="radix-:R2afekva:"]/div/div[1]/div[2]/div/input').send_keys(requests)
     driver.find_element(By.XPATH,'//*[@id="radix-:Rifekva:"]/div/div[1]/div[2]/div/input').send_keys(Keys.ENTER)
     WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.CSS_SELECTOR,'body > div.layout > div > div.SearchLayout_pills__dmJ3Q > div > a.SearchPills_pill__RiOen.SearchPills_activePill___xpgX')))
     element = driver.find_element(By.CSS_SELECTOR,'body > div.layout > div > div.SearchLayout_pills__dmJ3Q > div > a.SearchPills_pill__RiOen.SearchPills_activePill___xpgX')
